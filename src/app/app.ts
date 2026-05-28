aad8054283ba6720ff9660378661d84721109e6c
Algalgom <agalgom1303@g.educaand.es>
2026-05-28 10:14:14 +0200

    components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from '../components/header/header';
 import { Footer } from '../components/footer/footer';
 import { Home } from "../components/home/home";
+import { RouterOutlet } from "../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, Home],
+  imports: [Header, Footer, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
